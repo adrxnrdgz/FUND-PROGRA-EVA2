1c734c6162cc8f582a798ee32159ef22017969d3
adrxnrdgz <143474113+adrxnrdgz@users.noreply.github.com>
2023-10-30 16:47:48 -0700

    Add files via upload

---
+++ EVA2_17_WHILE/src/eva2_17_while/EVA2_17_WHILE.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_17_while;
+
+/**
+ *
+ * @author angel
+ */
+public class EVA2_17_WHILE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int i = 0;
+        while(i < 10){
+            System.out.println("Adrian!!");
+            i++;
+                    
+        
+        
+        }
+        
+    }
+    
+}
